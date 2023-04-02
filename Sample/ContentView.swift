27a5f764f5a9c8ed0635fe5b3d467c4d1f210580
104 <104developers@gmail.com>
2023-04-03 00:06:19 +0900

    Initial Commit

---
+++ Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by jushiro watanabe on 2023/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
